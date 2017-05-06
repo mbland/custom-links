390e296e6349ff8151e3d440b8b61c4d4e3a1548
Mike Bland <mbland@acm.org>
2017-05-06 12:03:09 +0200

    Add API router, first handler for /info
    Also contains minor internal refactorings to share the error handler
    implementation between the API router and the other app routes.

--- tests/app-test.js
+++ tests/app-test.js
@@ -13,12 +13,20 @@ chai.should()
 chai.use(chaiAsPromised)
 
 describe('assembleApp', function() {
-  var app, redirectDb, logger
+  var app, redirectDb, logger, logError
 
   before(function() {
     redirectDb = new RedirectDb
     logger = { error: function() { } }
     app = new assembleApp(express(), redirectDb, logger)
+  })
+
+  beforeEach(function() {
+    logError = sinon.spy(logger, 'error')
+  })
+
+  afterEach(function() {
+    logError.restore()
   })
 
   describe('get homepage and redirects', function() {
@@ -35,8 +43,7 @@ describe('assembleApp', function() {
     it('returns the index page', function() {
       return request(app)
         .get('/')
-        .expect(200)
-        .expect(/Url Pointers/)
+        .expect(200, /Url Pointers/)
     })
 
     it('redirects to the url returned by the RedirectDb', function() {
@@ -62,8 +69,6 @@ describe('assembleApp', function() {
     })
 
     it('reports an error', function() {
-      var logError = sinon.spy(logger, 'error')
-
       getRedirect.withArgs('/foo', { recordAccess: true })
         .callsFake(function() {
           return Promise.reject(new Error('forced error'))
@@ -71,12 +76,68 @@ describe('assembleApp', function() {
 
       return request(app)
         .get('/foo')
-        .expect(500)
-        .expect('Error while processing /foo: Error: forced error')
+        .expect(500, 'Internal Server Error')
         .then(function() {
           logError.calledOnce.should.be.true
           expect(logError.args[0][0].message).to.equal('forced error')
         })
+    })
+  })
+
+  describe('API', function() {
+    describe('unknown or malformed request', function() {
+      it('returns bad request', function() {
+        return request(app)
+          .get('/api')
+          .expect(400, 'Bad Request')
+      })
+    })
+
+    describe('/info', function() {
+      var getRedirect
+
+      beforeEach(function() {
+        getRedirect = sinon.stub(redirectDb, 'getRedirect')
+      })
+
+      afterEach(function() {
+        getRedirect.restore()
+      })
+
+      it('returns info for an existing URL', function() {
+        var urlData = {
+          location: 'https://mike-bland.com/', owner: 'mbland', count: 27 }
+        getRedirect.withArgs('/foo').returns(Promise.resolve(urlData))
+
+        return request(app)
+          .get('/api/info/foo')
+          .expect(200)
+          .then(function(response) {
+            response.body.should.eql(urlData)
+          })
+      })
+
+      it('returns not found', function() {
+        getRedirect.withArgs('/foo').returns(Promise.resolve(null))
+
+        return request(app)
+          .get('/api/info/foo')
+          .expect(404, 'Not Found')
+      })
+
+      it('returns server error', function() {
+        getRedirect.withArgs('/foo').callsFake(function() {
+          return Promise.reject(new Error('forced error'))
+        })
+
+        return request(app)
+          .get('/api/info/foo')
+          .expect(500, 'Internal Server Error')
+          .expect(function() {
+            logError.calledOnce.should.be.true
+            expect(logError.args[0][0].message).to.equal('forced error')
+          })
+      })
     })
   })
 })
