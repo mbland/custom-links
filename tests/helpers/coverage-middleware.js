465a239e89cd213bfbc864c9dd533e68357ef942
Mike Bland <mbland@acm.org>
2017-05-26 15:30:18 -0400

    test: Extract istanbul-middleware matcher function
    Doesn't make a functional difference, but better follows the API.

--- tests/helpers/coverage-middleware.js
+++ tests/helpers/coverage-middleware.js
@@ -8,13 +8,12 @@ var rootDir = path.dirname(path.dirname(__dirname))
 istanbulMiddleware.hookLoader(rootDir)
 
 module.exports = istanbulMiddleware.createClientHandler(rootDir, {
+  matcher(req) {
+    return /\.js$/.test(req.url) &&
+      ! (/\/(vendor|generated)\//.test(req.url) || /\.min\.js$/.test(req.url))
+  },
   pathTransformer(req) {
     var pathname = url.parse(req.url).pathname.substring(1)
-
-    if (/\/(vendor|generated)\//.test(pathname) ||
-        /\.min\.js$/.test(pathname)) {
-      return null
-    }
     return path.resolve(rootDir, 'public', pathname)
   }
 })
