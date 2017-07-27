1b5ca3b8039b1a00105bd71468db84d1521c3731
Mike Bland <mbland@acm.org>
2017-07-27 10:36:21 -0400

    Change [Uu][Rr][Ll][Pp] prefixes to [Cc][Ll]
    Should've been part of #85.

--- public/tests/lib.js
+++ public/tests/lib.js
@@ -2,16 +2,16 @@
 'use strict';
 
 (function(f) { f(window, document) })(function(window, document) {
-  var urlp = window.urlp
-  var urlpTest = window.urlpTest = {}
-  var fixture = urlpTest.fixture = document.createElement('div')
+  var cl = window.cl
+  var clTest = window.clTest = {}
+  var fixture = clTest.fixture = document.createElement('div')
 
   window.expect = window.chai.expect
   window.chai.should()
   window.chai.use(window.chaiAsPromised)
 
-  urlpTest.createFixture = function() {
-    return urlp.xhr('GET', '/index.html').then(function(xhr) {
+  clTest.createFixture = function() {
+    return cl.xhr('GET', '/index.html').then(function(xhr) {
       fixture.innerHTML = xhr.responseText
       fixture = fixture.getElementsByClassName('markup').item(0)
       fixture.className = 'fixture'
@@ -20,17 +20,17 @@
     })
   }
 
-  urlpTest.resetFixture = function() {
+  clTest.resetFixture = function() {
     var oldFixture = document.getElementsByClassName('fixture').item(0)
     oldFixture.parentNode.replaceChild(fixture.cloneNode(true), oldFixture)
   }
 
-  urlpTest.getView = function(viewClass) {
+  clTest.getView = function(viewClass) {
     return document.getElementsByClassName('view-container').item(0)
       .getElementsByClassName(viewClass)
   }
 
-  urlpTest.handleXhrError = function(description) {
+  clTest.handleXhrError = function(description) {
     return function(err) {
       console.log('failed to ' + description + ': ' + (err.message || err))
     }
@@ -38,11 +38,11 @@
 })
 
 before(function() {
-  return window.urlpTest.createFixture()
+  return window.clTest.createFixture()
     .then(function() {
       if (window.__coverage__) {
-        return window.urlp.xhr('POST', '/coverage/reset')
-          .catch(window.urlpTest.handleXhrError('clear coverage data'))
+        return window.cl.xhr('POST', '/coverage/reset')
+          .catch(window.clTest.handleXhrError('clear coverage data'))
       }
     })
     .catch(function(err) {
@@ -52,12 +52,12 @@ before(function() {
 })
 
 beforeEach(function() {
-  return window.urlpTest.resetFixture()
+  return window.clTest.resetFixture()
 })
 
 after(function() {
   if (window.__coverage__) {
-    return window.urlp.xhr('POST', '/coverage/client', window.__coverage__)
-      .catch(window.urlpTest.handleXhrError('post coverage data'))
+    return window.cl.xhr('POST', '/coverage/client', window.__coverage__)
+      .catch(window.clTest.handleXhrError('post coverage data'))
   }
 })
